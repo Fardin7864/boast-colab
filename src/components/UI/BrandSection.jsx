76ed7603b64fac33fab4426b5d18ca2bc8d3925d
Afsana Meem <afsumeem@gmail.com>
2024-03-14 15:41:00 +0600

    brand updated

--- src/components/UI/BrandSection.jsx
+++ src/components/UI/BrandSection.jsx
@@ -36,60 +36,47 @@ const BrandSection = () => {
     <div className="bg-black py-10">
       <div className="">
         <div className=" pt-3">
-          <Marquee>
+          <Marquee autoFill="true">
             <img
-              className=""
+              className=" mr-10"
               style={{ height: "80px", width: "100px" }}
               src="/images/logo1.png"
               alt="image1"
             />
 
             <img
-              style={{ height: "30px" }}
-              className="mx-10 "
+              style={{ height: "35px" }}
+              className=" mr-10"
               src="/images/logo2.png"
               alt="image2"
             />
 
             <img
-              style={{ height: "70px", width: "100px" }}
-              className="mx-10"
+              style={{ height: "100px", width: "90px" }}
+              className=" mr-10"
               src="/images/logo3.png"
               alt="image3"
             />
 
             <img
-              style={{ height: "80px", width: "90px" }}
-              className="mx-10"
+              style={{ height: "90px", width: "100px" }}
+              className=" mr-10"
               src="/images/logo4.png"
               alt="image4"
             />
 
             <img
               style={{ height: "30px" }}
-              className="mx-10"
+              className=" mr-10"
               src="/images/logo5.png"
               alt="image5"
             />
 
             <img
-              className="ml-20"
-              style={{ height: "80px", width: "90px" }}
+              className=" mr-10"
+              style={{ height: "100px", width: "90px" }}
               src="/images/logo6.png"
               alt="image6"
-            />
-            <img
-              style={{ height: "70px", width: "100px" }}
-              className="ml-20"
-              src="/images/logo3.png"
-              alt="image3"
-            />
-
-            <img
-              style={{ height: "80px", width: "90px" }}
-              className="mx-10"
-              src="/images/logo4.png"
-              alt="image4"
             />
           </Marquee>
         </div>
