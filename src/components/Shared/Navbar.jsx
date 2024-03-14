5222de15a7840653a04984734bbbf08efcae9719
Fardin7864 <fardinhasan1888@gmail.com>
2024-03-14 21:34:05 +0600

    changes on brand section

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -7,38 +7,18 @@ const Navbar = () => {
     wishlistSection.scrollIntoView({ behavior: "smooth" });
   };
   return (
-
-   
-       
-
     <div className=" drop-shadow-lg navbar">
       <div className="flex justify-between items-center mx-24 py-2">
-        {/* <h1
-          className="text-black text-4xl font-extrabold"
-          style={{ letterSpacing: "-6px" }}
-        >
-          BOAST
-        </h1> */}
-        {/* <img
-          style={{ height: "60px", width: "170px" }}
-          src="/images/logo.png"
-          alt=""
-        /> */}
         <Image
           src={"/images/1212.png"}
           width={100}
-
           height={0}
           alt="logo"
           className=""
         />
         <button
           onClick={handleScrollToWishlist}
-
-        
-
           className="bg-white hover:bg-[#fd7f3e] text-black font-bold px-6 py-2 rounded-full hover:text-black duration-300 "
-
         >
           Get Boast
         </button>
