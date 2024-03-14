82e66167d967ca04e5d566da8bad5b2cbbe04bfd
Afsana Meem <afsumeem@gmail.com>
2024-03-14 17:01:41 +0600

    hero updated

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
   };
   return (
     // <div className="flex justify-around mx-5 items-center pt-5 bg-slate-100">
-    <div className="flex justify-between items-center pt-5 bg-slate-100 mx-20">
+    <div className="flex justify-between items-center pt-5 bg-slate-100 mx-28">
       <div>
         <h2
           className="text-5xl font-extrabold"
