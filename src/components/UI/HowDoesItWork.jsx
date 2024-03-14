a1373522e5d1583be5464f2e491d8fd2b1b73d35
Fardin7864 <fardinhasan1888@gmail.com>
2024-03-14 13:51:59 +0600

    fix how does it work section

--- src/components/UI/HowDoesItWork.jsx
+++ src/components/UI/HowDoesItWork.jsx
@@ -3,6 +3,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import { FaStar } from "react-icons/fa6";
+import Image from "next/image";
 
 const HowDoesItWork = () => {
   useEffect(() => {
@@ -14,29 +15,102 @@ const HowDoesItWork = () => {
     <div>
       <div
         style={{ backgroundColor: "#a1df70" }}
-        className="flex flex-col justify-center lg:flex-row items-center p-20"
+        className="flex justify-center items-center py-20"
       >
-        <div className="w-1/2 m-5">
+        <div className="w-2/4">
           <h2
             data-aos="fade-right"
             data-aos-easing="ease-in-sine"
-            className="text-black text-center lg:text-start text-6xl font-extrabold "
-            style={{ letterSpacing: "-5px" }}
+            className="text-gray-900 text-center lg:text-start text-5xl font-extrabold "
+            style={{ letterSpacing: "-3px" }}
           >
             How Does it work?
           </h2>
+          <div data-aos="fade-left" className=" my-5">
+            <p className=" py-4 flex items-center gap-3">
+              <span className=" bg-[#fd7f3e] text-white p-3 rounded-full">
+                Stap-1 :
+              </span>
+              <span className=" text-2xl text-white"> Buy</span>{" "}
+              <span>
+                <Image
+                  src={"/right.webp"}
+                  width={20}
+                  height={20}
+                  alt="right arrow"
+                />
+              </span>{" "}
+              <span className=" bg-[#fd7f3e] text-white p-3 rounded-full">
+                Stap-2:
+              </span>{" "}
+              <span className=" text-2xl text-white">Post</span>{" "}
+              <Image
+                src={"/right.webp"}
+                width={20}
+                height={20}
+                alt="right arrow"
+              />{" "}
+              <span className=" bg-[#fd7f3e] text-white p-3 rounded-full">
+                Stap-3:
+              </span>{" "}
+              <span className=" text-2xl text-white">Earn</span>
+              <Image
+                src={"/hat.gif"}
+                width={100}
+                height={0}
+                alt="animat"
+                className=" rounded-2xl"
+              />{" "}
+            </p>
+            <div className="" data-aos="fade-left" data-aos-easing="linear">
+              <h2
+                className="text-black text-2xl flex items-center gap-2 my-3"
+                style={{ letterSpacing: "-2px" }}
+              >
+                <span className="text-sm mt-2">
+                  <FaStar className=" text-2xl text-orange-500" />
+                </span>
+                You become our member.
+              </h2>
+              <h2
+                className="text-black text-2xl flex items-center gap-2 my-3"
+                style={{ letterSpacing: "-2px" }}
+              >
+                <span className="text-sm mt-2">
+                  <FaStar className=" text-2xl text-orange-500" />
+                </span>
+                Shop from any of our Partner Brands and post it on social media.
+              </h2>
+              <h2
+                className="text-black text-2xl flex items-center gap-2 my-3"
+                style={{ letterSpacing: "-2px" }}
+              >
+                <span className="text-sm mt-2">
+                  <FaStar className=" text-2xl text-orange-500" />
+                </span>
+
+                <span>Get Cashback depending on you Boasting power.</span>
+              </h2>
+            </div>
+          </div>
         </div>
-        <div className="w-1/2">
-          <img
-            data-aos="fade-down"
-            data-aos-easing="linear"
-            style={{ height: "555px", width: "388px" }}
-            src="https://www.zywa.co/wp-content/uploads/2023/02/Rectangle-8.png"
-            alt=""
-          />
+        <div data-aos="fade-up" at className="w-1/3">
+          {/* <Image 
+          src={"/phone.png"}
+          width={500}
+          height={0}
+          alt="phone"
+          /> */}
+
+          <iframe
+            src="https://lottie.host/embed/256f9631-8f49-4c69-a550-378e2d780f30/AuDhWLtZ98.json"
+            width={500}
+            height={600}
+          ></iframe>
         </div>
-        <div
-          className="w-1/2 m-5"
+
+        {/* <div
+          className=""
           data-aos="fade-left"
           data-aos-easing="linear"
         >
@@ -68,7 +142,7 @@ const HowDoesItWork = () => {
 
             <span>Get Cashback depending on you Boasting power.</span>
           </h2>
-        </div>
+        </div> */}
       </div>
     </div>
   );
